a8e457080da77e89d2defc7e0980080ab756a755
Dekoi Dev <100128850+emepuchades@users.noreply.github.com>
2022-06-01 01:26:49 +0200

    Added top nav

--- src/components/NavTop.js
+++ src/components/NavTop.js
@@ -1,15 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
+import Paper from '@mui/material/Paper';
+import InputBase from '@mui/material/InputBase';
+import IconButton from '@mui/material/IconButton';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBell, faUser, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 
 
 function NavTop() {
+    const [search, setSearch] = useState('')
+
+    const handleInputChange = (event) => {
+        setSearch(event.target.value)
+    }
+
     return (
         <Block>
             <Route>Inicio</Route>
             <RigthBlock>
-                <Search>Search</Search>
-                <Notifications>Notificaciones</Notifications>
-                <User>User</User>
+                <Search>
+                    <Paper
+                        component="form"
+                        className='paper'
+                        sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 250, height: 30, boxShadow: 'none' }}
+                    >
+                        <InputBase
+                            sx={{ ml: 1, flex: 1 }}
+                            placeholder="Buscar"
+                        />
+                        <IconButton type="submit" sx={{ p: '10px' }} aria-label="search">
+                            <FontAwesomeIcon className="icon-search" icon={faMagnifyingGlass} />
+                        </IconButton>
+                    </Paper>
+                </Search>
+                <Notifications>
+                    <FontAwesomeIcon icon={faBell} />
+                </Notifications>
+                <User>
+                    <FontAwesomeIcon icon={faUser} />
+                </User>
             </RigthBlock>
         </Block>
     )
@@ -35,13 +64,21 @@ const RigthBlock = styled.div`
     float: rigth;
 `;
 const Search = styled.div`
-    margin-right: 30px;
+    margin-right: 60px;
+
+    .icon-search{
+        width: 18px;
+    }
 `;
 const Notifications = styled.div`
-    margin-right: 30px;
+    display: flex;
+    align-items: center;
+    margin-right: 60px;
 `;
 const User = styled.div`
-    margin-right: 80px;
+    display: flex;
+    align-items: center;
+    margin-right: 60px;
 `;
 
 export default NavTop
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import styled from 'styled-components'
-import NavLeft from '../components/NavLeft';
 
 function Dashboard() {
     return (
