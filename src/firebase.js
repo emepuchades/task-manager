dcdef3d221a9e4bd69aff27ae894f729a153a37b
Sergio Kepo <100128850+emepuchades@users.noreply.github.com>
2022-10-07 11:41:00 +0200

    fix-firebase

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,12 @@ import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   // Your configuration and your keys
-  apiKey: "AIzaSyDRkJLpp7BpUMbdXkKBOvS2l2eKarEcCBg",
-  authDomain: "task-manager-fb028.firebaseapp.com",
-  projectId: "task-manager-fb028",
-  storageBucket: "task-manager-fb028.appspot.com",
-  messagingSenderId: "950208037476",
-  appId: "1:950208037476:web:6bc5d256d13e7ec792d571",
-  measurementId: "G-LXT9XW79JV"
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINGID,
+  appId: process.env.APPID
 };
 
 const app = initializeApp(firebaseConfig);
